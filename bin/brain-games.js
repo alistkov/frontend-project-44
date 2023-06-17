4e2b287e1e560cb9cfb2a0e34db33cac172e7103
Anton Listkov <antonlistkov@gmail.com>
2023-06-17 20:18:40 +0300

    add prettier config

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!");
-const name = readlineSync.question("May I have your name? ");
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
