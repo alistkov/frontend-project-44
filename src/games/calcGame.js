ab797e19feb5e2999faaa9b5f3b4cd9edbd5d7f1
Anton Listkov <antonlistkov@gmail.com>
2023-06-19 19:20:46 +0300

    refactor brain calc game

--- src/games/calcGame.js
+++ src/games/calcGame.js
@@ -1,34 +1,24 @@
-import readlineSync from 'readline-sync';
-import { generateRandomNumber, getOperationResult } from '../helpers.js';
+import runGame from '../index.js';
+import {
+  generateRandomNumber,
+  getOperationResult,
+  roundsCount,
+} from '../helpers.js';
 
 const createCalcGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('What is the result of the expression?');
-  let steps = 3;
+  const rules = 'What is the result of the expression?';
+  const gameData = [];
   const operations = ['+', '-', '*'];
-  while (steps > 0) {
+  for (let i = 1; i <= roundsCount; i += 1) {
     const first = generateRandomNumber(1, 30);
     const second = generateRandomNumber(1, 30);
     const operationIndex = generateRandomNumber(0, 2);
     const operation = operations[operationIndex];
-
-    console.log(`Question: ${first} ${operation} ${second}`);
-    const userAnswer = Number(readlineSync.question('Your answer: '));
+    const question = `${first} ${operation} ${second}`;
     const correctAnswer = getOperationResult(first, second, operation);
-
-    if (userAnswer !== correctAnswer) {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-      );
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-    console.log('Correct!');
-    steps -= 1;
+    gameData.push([question, String(correctAnswer)]);
   }
-  console.log(`Congratulations, ${name}!`);
+  runGame(rules, gameData);
 };
 
 export default createCalcGame;
